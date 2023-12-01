6c0d72bebb7b14a53183b13d26a73aa47867a598
Rokas <rookas.rudzenskas@gmail.com>
2023-12-01 15:36:02 +0200

    calendar

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
-import {StyleSheet, Text, View} from "react-native";
+// @ts-nocheck
+import {FlatList, StyleSheet, Text, View} from "react-native";
 import React from "react";
+
+const DAYS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, , 17, 18, 19, 20, 21, 22, 23, 24];
 
 const TabOneScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>1</Text>
-      </View>
+      <FlatList
+        data={DAYS}
+        numColumns={4}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item!}
+        renderItem={({item}) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -17,15 +28,14 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    gap: 10,
+    gap: 3,
     alignItems: 'center',
     justifyContent: 'center',
   },
   box: {
     backgroundColor: '#F9EDE3',
-    width: 300,
-    height: 300,
-
+    width: 80,
+    height: 80,
     borderWidth: StyleSheet.hairlineWidth,
     borderColor: '#9b4521',
     borderRadius: 20,
