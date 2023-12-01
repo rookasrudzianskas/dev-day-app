c2cc94a1fd8fd6dd36eff740cf238ccfc669b1ea
Rokas <rookas.rudzenskas@gmail.com>
2023-12-01 16:04:05 +0200

    format number font

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatNumber = (number: number) => {
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(number)
+}
