a32a5bb1403be0ae0472ac2615ab767722c412a7
Rokas <rookas.rudzenskas@gmail.com>
2023-12-01 15:22:10 +0200

    Fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,12 @@
+// @ts-nocheck
 import { Text, View } from "react-native";
 import React from "react";
 
 const TabOneScreen = () => {
   return (
-    <View className="bg-gray-100 flex-1 p-3">
-      <View className="flex-1 bg-white rounded-xl pt-6 items-center justify-center">
-        <Text className="text-2xl uppercase font-bold text-gray-700 text-center">
-          Nativewind v4 starter
-        </Text>
+    <View className="">
+      <View clasName="w-16 h-10 bg-red-500">
+        <Text>Rokas</Text>
       </View>
     </View>
   );
