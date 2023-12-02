0fad4a5f7308071c44617c4ca7d6cb82478e8df9
Rokas <rookas.rudzenskas@gmail.com>
2023-12-02 14:41:19 +0200

    fix

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -8,14 +8,16 @@ const Onboarding = () => {
   return (
     <View className="pt-16 bg-[#15141A] h-screen items-center justify-center">
       <Stack.Screen options={{ headerShown: false}} />
-      <View className="bg-[#15141A] flex items-start gap-y-2 justify-center shadow-2xl h-full my-10 mx-6 rounded-xl">
-        <FontAwesome name="money" size={40} color="#FDFDFD" />
-        <Text style={styles.title} className="text-gray-200">
-          Track Every Penny
-        </Text>
-        <Text className="text-gray-300 text-lg">
-          Monitor your spending and contribution ensuring every penny is accounted for. This will help you to make better financial decisions.
-        </Text>
+      <View className="bg-[#15141A] h-full justify-between">
+        <FontAwesome style={styles.image} name="money" size={40} color="#FDFDFD" />
+          <View className="mb-16">
+            <Text style={styles.title} className="text-gray-200">
+              Track Every Penny
+            </Text>
+            <Text className="text-gray-300 text-lg spacing-wide">
+              Monitor your spending and contribution ensuring every penny is accounted for. This will help you to make better financial decisions.
+            </Text>
+          </View>
       </View>
     </View>
   );
@@ -32,7 +34,19 @@ const styles = StyleSheet.create({
   },
   title: {
     fontFamily: 'Inter',
-    fontSize: 26,
+    fontSize: 35,
+    marginBottom: 10,
     letterSpacing: 1.3,
+  },
+  image: {
+    fontFamily: 'Inter',
+    fontSize: 60,
+    letterSpacing: 1.3,
+    alignSelf: 'center',
+    margin: 20,
+    marginTop: 50,
+  },
+  footer: {
+    margin: 'auto'
   }
 });
