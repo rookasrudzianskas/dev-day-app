90d493186c546ad661a03b4838e1900bfa2f3236
Rokas <rookas.rudzenskas@gmail.com>
2023-12-04 14:24:39 +0200

    animation added

--- src/app/(days)/day4/animation.tsx
+++ src/app/(days)/day4/animation.tsx
@@ -5,23 +5,36 @@ import LottieView from 'lottie-react-native';
 
 const Animation = () => {
   const animation = useRef(null);
-  useEffect(() => {
-    // You can control the ref programmatically, rather than using autoPlay
-    // animation.current?.play();
-  }, []);
+
+  // useEffect(() => {
+  //   // You can control the ref programmatically, rather than using autoPlay
+  //   animation.current?.play();
+  // }, []);
 
   return (
     <View style={styles.animationContainer}>
       <LottieView
-        autoPlay
+        // autoPlay
         ref={animation}
         style={{
           width: 200,
           height: 200,
           backgroundColor: '#eee',
         }}
-        // Find more Lottie files at https://lottiefiles.com/featured
         source={require('../../../assets/animation.json')}
+      />
+      <Button
+        title="Restart Animation"
+        onPress={() => {
+          animation.current?.reset();
+          animation.current?.play();
+        }}
+      />
+      <Button
+        title="Play"
+        onPress={() => {
+          animation.current?.play();
+        }}
       />
     </View>
   );
@@ -31,7 +44,7 @@ export default Animation;
 
 const styles = StyleSheet.create({
   animationContainer: {
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
