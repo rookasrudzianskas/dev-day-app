6fc389cfa238181695aff5d88c2ab2cf29366174
Rokas <rookas.rudzenskas@gmail.com>
2023-12-04 14:06:53 +0200

    fix

---
+++ src/app/(days)/day4/index.tsx
@@ -0,0 +1,17 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {Stack} from "expo-router";
+
+const Day4 = () => {
+  return (
+    <View>
+      <Stack.Screen options={{ title: 'Day 4 Splash Screens'}} />
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default Day4;
