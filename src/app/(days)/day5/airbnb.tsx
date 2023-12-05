518714ac44d436ad15e8ebe3781071f52689a1b6
Rokas <rookas.rudzenskas@gmail.com>
2023-12-05 14:29:46 +0200

    fix

--- src/app/(days)/day5/airbnb.tsx
+++ src/app/(days)/day5/airbnb.tsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Stack} from "expo-router";
-import MapView from "react-native-maps";
-
+import MapView, {Marker} from "react-native-maps";
+import {APARTMENTS} from "@/src/assets/data/day5/data";
 const Airbnb = () => {
   return (
     <>
@@ -13,12 +13,29 @@ const Airbnb = () => {
           style={styles.map}
           provider="google"
           initialRegion={{
-            latitude: 54.792583,
-            longitude: 25.343330,
+            latitude: 54.6896,
+            longitude: 25.2799,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-        />
+        >
+          {APARTMENTS.map((apartment, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: apartment.latitude,
+                longitude: apartment.longitude,
+            }}
+              title={apartment.title}
+              description={`$${apartment.price} / night`}
+              // image={{uri: 'custom_pin'}}
+            >
+              <View>
+                <Text>{apartment.price}</Text>
+              </View>
+            </Marker>
+          ))}
+        </MapView>
       </View>
     </>
   );
@@ -27,9 +44,6 @@ const Airbnb = () => {
 export default Airbnb;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
     width: '100%',
     height: '100%',
---
+++ src/assets/data/day5/data.ts
@@ -0,0 +1,58 @@
+export const APARTMENTS = [
+  {
+    latitude: 54.6896,
+    longitude: 25.2799,
+    price: 90,
+    title: "Spacious Apartment in Old Town Center",
+    numberOfStars: 4,
+    rating: 8.9,
+    image: "https://www.thespruce.com/thmb/iMt63n8NGCojUETr6-T8oj-5-ns=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/PAinteriors-7-cafe9c2bd6be4823b9345e591e4f367f.jpg"
+  },
+  {
+    latitude: 54.6898,
+    longitude: 25.2805,
+    price: 110,
+    title: "Luxury Loft with Panoramic View",
+    numberOfStars: 5,
+    rating: 9.7,
+    image: "https://www.usnews.com/object/image/00000167-32b5-dff7-a56f-7bb53c640000/181120-bedroom-stock.jpg?update-time=1542744061266&size=responsive640"
+  },
+  {
+    latitude: 54.6842,
+    longitude: 25.2791,
+    price: 75,
+    title: "Cozy Studio near Gediminas Avenue",
+    numberOfStars: 3,
+    rating: 7.5,
+    image: "https://www.erfurt.com/fileadmin/user_upload/tipps-inspirationen/tipps-tricks/raumwirkung/Leerer-Raum-graue-front-decke_thumb_gross.jpg"
+  },
+  {
+    latitude: 54.6784,
+    longitude: 25.2783,
+    price: 95,
+    title: "Modern Apartment with Garden Access",
+    numberOfStars: 4,
+    rating: 8.3,
+    image: "https://www.aestheticroomcore.com/cdn/shop/files/aesthetic_lighting_2_300x.jpg?v=1628246281"
+  },
+  {
+    latitude: 54.6891,
+    longitude: 25.2719,
+    price: 120,
+    title: "Elegant Penthouse in Vilnius Center",
+    numberOfStars: 5,
+    rating: 9.8,
+    image: "https://www.thespruce.com/thmb/2_Q52GK3rayV1wnqm6vyBvgI3Ew=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/put-together-a-perfect-guest-room-1976987-hero-223e3e8f697e4b13b62ad4fe898d492d.jpg"
+  },
+  {
+    latitude: 54.6953,
+    longitude: 25.2812,
+    price: 85,
+    title: "Rustic Apartment in Artist's Quarter",
+    numberOfStars: 4,
+    rating: 8.6,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Imperial_Hotel_Osaka_regular_floor_standard_twin_room_20120630-001.jpg/640px-Imperial_Hotel_Osaka_regular_floor_standard_twin_room_20120630-001.jpg"
+  }
+  // Add more apartments as needed
+];
+
