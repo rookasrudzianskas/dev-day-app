dded1eef1b9b42ad4265af390e010adc1118c055
Rokas <rookas.rudzenskas@gmail.com>
2023-12-07 17:00:28 +0200

    fix:

--- src/components/day7/memo-list-item.tsx
+++ src/components/day7/memo-list-item.tsx
@@ -1,49 +1,59 @@
-//@ts-nocheck
-import React, {useCallback, useEffect, useState} from 'react';
-import {Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
-import {Audio, AVPlaybackStatus} from "expo-av";
+// @ts-nocheck
+import { View, Text, StyleSheet } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
-import {useAnimatedStyle, withTiming} from "react-native-reanimated";
-import Animated from "react-native-reanimated";
+import { useState, useEffect, useCallback } from 'react';
+import { AVPlaybackStatus, Audio } from 'expo-av';
+import { Sound } from 'expo-av/build/Audio';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
 
-const MemoListItem = ({uri}: {uri: string}) => {
-  const [sound, setSound] = useState();
-  const [status, setStatus] = useState<AVPlaybackStatus | null>(null);
+export type Memo = {
+  uri: string;
+  metering: number[];
+};
 
-  const loadSound = async () => {
-    console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync({
-      uri: uri,
-    },
-      { progressUpdateIntervalMillis: 50 },
+const MemoListItem = ({ memo }: { memo: Memo }) => {
+  const [sound, setSound] = useState<Sound>();
+  const [status, setStatus] = useState<AVPlaybackStatus>();
+
+  async function loadSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      { uri: memo.uri },
+      { progressUpdateIntervalMillis: 1000 / 60 },
       onPlaybackStatusUpdate
     );
     setSound(sound);
   }
 
+  const onPlaybackStatusUpdate = useCallback(
+    async (newStatus: AVPlaybackStatus) => {
+      setStatus(newStatus);
+
+      if (!newStatus.isLoaded || !sound) {
+        return;
+      }
+
+      if (newStatus.didJustFinish) {
+        await sound?.setPositionAsync(0);
+      }
+    },
+    [sound]
+  );
+
   useEffect(() => {
     loadSound();
-  }, [uri]);
-
-  const onPlaybackStatusUpdate = async(newStatus: AVPlaybackStatus) => {
-    setStatus(newStatus);
-
-    // if (!newStatus.isLoaded || !sound) {
-    //   return;
-    // }
-
-    if (newStatus.didJustFinish) {
-      // sound?.replayAsync();
-      await sound?.setPositionAsync(0);
-    }
-  }
+  }, [memo]);
 
   async function playSound() {
-    if(!sound) return;
-    console.log('Playing Sound');
-
-    if(status?.isLoaded && status?.isPlaying) {
-      await sound.stopAsync();
+    if (!sound) {
+      return;
+    }
+    if (status?.isLoaded && status.isPlaying) {
+      await sound.pauseAsync();
     } else {
       await sound.replayAsync();
     }
@@ -58,80 +68,141 @@ const MemoListItem = ({uri}: {uri: string}) => {
       : undefined;
   }, [sound]);
 
-  const isPlaying = status?.isLoaded ? status.isPlaying : false
-  const progress = status?.isLoaded ? status.positionMillis / status.durationMillis * 100 : 0;
-  const duration = status?.isLoaded ? `${Math.floor(status.durationMillis / 1000 / 60)}:${Math.floor(status.durationMillis / 1000 % 60)}` : '0:00';
+  const formatMillis = (millis: number) => {
+    const minutes = Math.floor(millis / (1000 * 60));
+    const seconds = Math.floor((millis % (1000 * 60)) / 1000);
+
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  const isPlaying = status?.isLoaded ? status.isPlaying : false;
+  const position = status?.isLoaded ? status.positionMillis : 0;
+  const duration = status?.isLoaded ? status.durationMillis : 1;
+
+  const progress = position / duration;
 
   const animatedIndicatorStyle = useAnimatedStyle(() => ({
-    left: `${progress}%`,
-    // left: withTiming(``,
-    //   {
-    //     duration: (status?.isLoaded && status.progressUpdateIntervalMillis) || 100,
-    //   }),
-  }))
+    left: `${progress * 100}%`,
+    // withTiming(`${progress * 100}%`, {
+    //   duration:
+    //     (status?.isLoaded && status.progressUpdateIntervalMillis) || 100,
+    // }),
+  }));
+
+  let numLines = 50;
+  let lines = [];
+
+  for (let i = 0; i < numLines; i++) {
+    const meteringIndex = Math.floor((i * memo?.metering?.length) / numLines);
+    const nextMeteringIndex = Math.ceil(
+      ((i + 1) * memo?.metering?.length) / numLines
+    );
+    const values = memo?.metering?.slice(meteringIndex, nextMeteringIndex);
+    const average = values.reduce((sum, a) => sum + a, 0) / values.length;
+    // lines.push(memo.metering[meteringIndex]);
+    lines.push(average);
+  }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        activeOpacity={0.7}
+      <FontAwesome5
         onPress={playSound}
-        style={{marginRight: 15}}
-      >
-        <FontAwesome5 name={
-          isPlaying
-            ? 'pause-circle'
-            : 'play-circle'
-        }
+        name={isPlaying ? 'pause' : 'play'}
         size={20}
-        color="gray" />
-      </TouchableOpacity>
+        color={'gray'}
+      />
+
       <View style={styles.playbackContainer}>
-        <View style={styles.playbackBackground}/>
-        <Animated.View style={[styles.playbackIndicator, animatedIndicatorStyle]}/>
-        <Text className="absolute right-0 -bottom-1 text-gray-400 font-semibold text-xs">{duration}</Text>
+        {/* <View style={styles.playbackBackground} /> */}
+
+        <View style={styles.wave}>
+          {lines.map((db, index) => (
+            <View
+              style={[
+                styles.waveLine,
+                {
+                  height: interpolate(db, [-60, 0], [5, 50], Extrapolate.CLAMP),
+                  backgroundColor:
+                    progress > index / lines.length ? 'royalblue' : 'gainsboro',
+                },
+              ]}
+            />
+          ))}
+        </View>
+
+        {/* <Animated.View
+          style={[styles.playbackIndicator, animatedIndicatorStyle]}
+        /> */}
+
+        <Text
+          style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+            color: 'gray',
+            fontFamily: 'Inter',
+            fontSize: 12,
+          }}
+        >
+          {formatMillis(position || 0)} / {formatMillis(duration || 0)}
+        </Text>
       </View>
     </View>
   );
 };
 
-export default MemoListItem;
-
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     margin: 5,
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 15,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
     borderRadius: 10,
     gap: 15,
-    shadowColor: "#000",
+
+    // shadow
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
 
-    elevation: 5,
+    elevation: 3,
   },
+
   playbackContainer: {
-    // backgroundColor: 'gray',
     flex: 1,
-    height: 30,
+    height: 80,
     justifyContent: 'center',
   },
   playbackBackground: {
-    backgroundColor: 'gainsboro',
     height: 3,
-    width: '100%',
+    backgroundColor: 'gainsboro',
     borderRadius: 5,
   },
   playbackIndicator: {
     width: 10,
     aspectRatio: 1,
-    backgroundColor: 'royalblue',
     borderRadius: 10,
+    backgroundColor: 'royalblue',
     position: 'absolute',
-  }
-})
+  },
+
+  wave: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 3,
+  },
+  waveLine: {
+    flex: 1,
+    height: 30,
+    backgroundColor: 'gainsboro',
+    borderRadius: 20,
+  },
+});
+
+export default MemoListItem;
