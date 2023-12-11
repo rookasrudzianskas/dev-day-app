858a6321271c6daa24888c798fe558f1f9909a09
Rokas <rookas.rudzenskas@gmail.com>
2023-12-11 09:15:25 +0200

    auth

--- src/app/(days)/day9/protected/index.tsx
+++ src/app/(days)/day9/protected/index.tsx
@@ -7,8 +7,8 @@ const ProtectedScreen = () => {
 
   return (
     <View style={{ padding: 10 }}>
-      <Text style={{ fontFamily: 'InterBold', fontSize: 30 }}>Hello there</Text>
-      <Text style={{ fontFamily: 'InterSemi', fontSize: 20, color: 'gray' }}>
+      <Text style={{ fontFamily: 'Inter', fontSize: 30 }}>Hello there</Text>
+      <Text style={{ fontFamily: 'Inter', fontSize: 20, color: 'gray' }}>
         You should see this page only if you are Authenticated
       </Text>
 
