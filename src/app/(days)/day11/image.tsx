1b84389dab4ba948656032dd17804550085588eb
Rokas <rookas.rudzenskas@gmail.com>
2023-12-11 16:40:48 +0200

    fix

---
+++ src/app/(days)/day11/image.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Image = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default Image;
