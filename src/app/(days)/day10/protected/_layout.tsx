410936ea05e76727a0c3cb892fd548da6937a7c3
Rokas <rookas.rudzenskas@gmail.com>
2023-12-11 14:38:03 +0200

    fix

--- src/app/(days)/day10/protected/_layout.tsx
+++ src/app/(days)/day10/protected/_layout.tsx
@@ -1,16 +1,43 @@
-import {Slot} from "expo-router";
-import {useEffect} from "react";
-import {authenticateAsync} from "expo-local-authentication";
+import { Slot } from 'expo-router';
+import { useEffect, useState } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
+import { Alert, Text, View } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import {useBiometrics} from "@/src/components/day10/biometrics-provider";
 
 export default function BiometricProtectedLayout() {
+  const { isUnlocked, authenticate } = useBiometrics();
 
   useEffect(() => {
-    const authenticate = async () => {
-      const res = await authenticateAsync();
-      console.log(res);
+    console.log(isUnlocked)
+    if (!isUnlocked) {
+      authenticate();
+      console.log('authenticating')
     }
-    authenticate();
   }, []);
 
-  return (<Slot />)
+  if (!isUnlocked) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'white',
+        }}
+      >
+        <Text style={{ fontFamily: 'Inter', fontSize: 20, marginBottom: 20 }}>
+          Use FaceId to Unlock
+        </Text>
+        <FontAwesome5
+          onPress={authenticate}
+          name="fingerprint"
+          size={75}
+          color="gray"
+        />
+      </View>
+    );
+  }
+
+  return <Slot />;
 }
--- src/app/(days)/day10/protected/index.tsx
+++ src/app/(days)/day10/protected/index.tsx
@@ -1,13 +1,24 @@
-//@ts-nocheck
+import { View, Text } from 'react-native';
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 const ProtectedScreen = () => {
   return (
-    <View>
-      <Text>
-        This is a private info!
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}
+    >
+      <Text style={{ fontFamily: 'Inter', fontSize: 20, marginBottom: 20 }}>
+        Protected Info
       </Text>
+      <FontAwesome5 name="lock" size={75} color="gray" />
+
+      <Link href={'/day10/protected/second'}>Next page</Link>
     </View>
   );
 };
