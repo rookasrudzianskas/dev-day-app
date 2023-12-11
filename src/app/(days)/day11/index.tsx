fe9436cfbd803c3acd249595f06735381df1f8ef
Rokas <rookas.rudzenskas@gmail.com>
2023-12-11 15:06:43 +0200

    fix

---
+++ src/app/(days)/day11/index.tsx
@@ -0,0 +1,34 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet, Button, SafeAreaView} from 'react-native';
+import {Stack, useRouter} from "expo-router";
+import MarkdownDisplay from "@/src/components/day3/markdown-display";
+
+const description = `
+# Camera App
+`;
+
+const Day11 = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: 'Day 11 Camera'}} />
+      <MarkdownDisplay>
+        {description}
+      </MarkdownDisplay>
+      <Button
+        title={'Go To Editor'}
+        className="mb-6"
+        onPress={() => router.push('/day11/camera')}>
+        Go To Editor
+      </Button>
+    </SafeAreaView>
+  );
+};
+
+export default Day11;
+
+export const styles = StyleSheet.create({
+
+});
