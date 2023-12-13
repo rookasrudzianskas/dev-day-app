c9c4fcc4eff1ee447de625d5f23ca3bc929c64b2
Rokas <rookas.rudzenskas@gmail.com>
2023-12-13 14:17:57 +0200

    fix

---
+++ src/app/(days)/day13/index.tsx
@@ -0,0 +1,34 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet, Button, SafeAreaView} from 'react-native';
+import {Stack, useRouter} from "expo-router";
+import MarkdownDisplay from "@/src/components/day3/markdown-display";
+
+const description = `
+# EAS BUILD
+`;
+
+const Day12 = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: 'Day 13 EAS'}} />
+      <MarkdownDisplay>
+        {description}
+      </MarkdownDisplay>
+      <Button
+        title={'Go To EAS'}
+        className="mb-6"
+        onPress={() => router.push('/day13/feed')}>
+        Go To TikTok
+      </Button>
+    </SafeAreaView>
+  );
+};
+
+export default Day12;
+
+export const styles = StyleSheet.create({
+
+});
