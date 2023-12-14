b80478a4c3fd00cea6733d718499edbd4d07fac8
Rokas <rookas.rudzenskas@gmail.com>
2023-12-14 16:11:07 +0200

    fix

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -27,8 +27,6 @@ P
  src/app/(days)/day11/_layout.tsx,7/c/7ce5250c5f7986e2584dfad9d094ab0368be3895
 N
 src/app/(days)/day11/image.tsx,2/8/28da733fc4bfa5245523a991801a5696da1fce32
-8
-app.json,6/5/65596286f14dbaba6a5081a2edaae69700cc3c3b
 N
 src/app/(days)/day12/index.tsx,5/0/500ecb2e852c6f1b18d99ff9d7d17e40be66845b
 S
--- src/app/(days)/day14/notifications/_layout.tsx
+++ src/app/(days)/day14/notifications/_layout.tsx
@@ -6,6 +6,7 @@ import {Text, View, Button, Platform, Alert} from 'react-native';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import {alert} from "@aws-amplify/ui/dist/types/theme/tokens/components/alert";
+import * as Constants from "expo-constants";
 // import {registerForPushNotificationsAxsync} from "@/src/app/lib/register-for-push-notifications-async";
 //
 Notifications.setNotificationHandler({
@@ -104,9 +105,11 @@ async function registerForPushNotificationsAsync() {
       Alert.alert('Failed to get push token for push notification!');
       return;
     }
-    // Learn more about projectId:
-    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-    token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
+
+    if(!Constants.expoConfig.extra.eas.projectId) return;
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: Constants.expoConfig.extra.eas.projectId,
+    });
     console.log(token);
   } else {
     Alert.alert('Must use physical device for Push Notifications');
