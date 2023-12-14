bdae817c77695d18fdb5d5334e8991baaa06d874
Rokas <rookas.rudzenskas@gmail.com>
2023-12-14 13:55:25 +0200

    fix

---
+++ src/app/(days)/day14/index.tsx
@@ -0,0 +1,30 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet, Button, SafeAreaView} from 'react-native';
+import {Stack, useRouter} from "expo-router";
+import MarkdownDisplay from "@/src/components/day3/markdown-display";
+
+const description = `
+# Expo Notifications
+`;
+
+const Day14 = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: 'Day 14 Notifications'}} />
+      <MarkdownDisplay>
+        {description}
+      </MarkdownDisplay>
+      <Button
+        title={'Go To Notifications'}
+        className="mb-6"
+        onPress={() => router.push('/day13/feed')}>
+        Go To Notifications
+      </Button>
+    </SafeAreaView>
+  );
+};
+
+export default Day14;
