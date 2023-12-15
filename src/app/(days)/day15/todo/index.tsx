144001f40fce867816cae9658361917cab39dee9
Rokas <rookas.rudzenskas@gmail.com>
2023-12-15 16:35:14 +0200

    fix

--- src/app/(days)/day15/todo/index.tsx
+++ src/app/(days)/day15/todo/index.tsx
@@ -1,11 +1,35 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import { Stack } from 'expo-router';
 
+const TASKS = [
+  {
+    id: 1,
+    task: 'Learn React Native',
+    isFinished: false
+  },
+  {
+    id: 2,
+    task: 'Learn React',
+    isFinished: false
+  },
+  {
+    id: 3,
+    task: 'Learn TypeScript',
+    isFinished: false
+  },
+  {
+    id: 4,
+    task: 'Learn JavaScript',
+    isFinished: false
+  }
+]
+
 const TodoScreen = () => {
+  const [tasks, setTasks] = useState(TASKS);
   return (
-    <View>
+    <View style={styles.page}>
       <Stack.Screen options={{ title: 'TODO' }} />
       <Text>
         byrookas 🚀
@@ -15,3 +39,9 @@ const TodoScreen = () => {
 };
 
 export default TodoScreen;
+
+const styles = StyleSheet.create({
+  page: {
+
+  }
+})
