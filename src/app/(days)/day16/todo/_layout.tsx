e0b9cbbbe8a93e61d07140dbf3597e9109ee5ba7
Rokas <rookas.rudzenskas@gmail.com>
2023-12-16 15:41:00 +0200

    fix

--- src/app/(days)/day16/todo/_layout.tsx
+++ src/app/(days)/day16/todo/_layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Slot, Stack} from "expo-router";
+import TasksContextProvider from "@/src/components/day16/TasksContextProvider";
 
 export default function TodoLayout() {
   return (
-    <>
+    <TasksContextProvider>
       <Stack.Screen options={{ headerShown: false }} />
       <Stack />
-    </>
+    </TasksContextProvider>
   )
 }
