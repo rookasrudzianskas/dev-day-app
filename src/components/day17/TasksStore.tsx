8180d0e073d07dbdb0edae704c7fddfdf591223e
Rokas <rookas.rudzenskas@gmail.com>
2023-12-17 15:59:16 +0200

    fix

--- src/components/day17/TasksStore.tsx
+++ src/components/day17/TasksStore.tsx
@@ -19,67 +19,12 @@ type TasksStore = {
 }
 
 const useTasksStore = create<TasksStore>(
-  persist((set, get) => ({
-  tasks: dummyTasks,
-  numberOfCompletedTasks: () => {
-    const tasks = get().tasks;
-    return tasks.filter((task) => task.isFinished).length;
-  },
-  numberOfTasks: () => {
-    const tasks = get().tasks;
-    return tasks.length;
-  },
-  addTask: (title: string) => {
-    const newTask: Task = {
-      id: Math.random().toString(),
-      title,
-      isFinished: false
-    }
-    set((state: any) => (
-      {
-        tasks: [...state.tasks, newTask]
-      }
-      )
-    );
-  },
-  deleteTask: (id: string) => {
-    set((state: any) => (
-      {
-        tasks: state.tasks.filter((task: Task) => task.id !== id)
-      }
-    ))
-  },
-  changeIsFinished: (id: string) => {
-    set((state: any) => (
-      {
-        tasks: state.tasks.map((task: Task) => task.id === id ? {...task, isFinished: !task.isFinished} : task)
-      }
-    ))
-  },
-  getFilteredTasks: (tab: string, searchQuery: string) => {
-    const tasks = get().tasks;
-    return tasks.filter((task: Task) => {
-      if (task.isFinished && tab === 'Todo') {
-        return false;
-      }
-      if (!task.isFinished && tab === 'Finished') {
-        return false;
-      }
-
-      if (!searchQuery) {
-        return true;
-      }
-
-      return task.title
-        .toLowerCase()
-        .trim()
-        .includes(searchQuery.toLowerCase().trim());
-    });
-  }
-}), {
+  persist(
     name: 'tasks',
     storage: createJSONStorage(() => sessionStorage),
   })
 );
+
+
 
 export default useTasksStore
