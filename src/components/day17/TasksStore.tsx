37d8eb4083d11eb2e54bbf267d88559ac570f9bb
Rokas <rookas.rudzenskas@gmail.com>
2023-12-17 14:45:34 +0200

    fix

--- src/components/day17/TasksStore.tsx
+++ src/components/day17/TasksStore.tsx
@@ -1,9 +1,17 @@
 import { create } from 'zustand'
 import {dummyTasks} from "@/src/components/day17/data";
+import {Task} from "@/src/components/day17/TasksContextProvider";
 
 const useTasksStore = create((set, get) => ({
   tasks: dummyTasks,
-
+  addTask: (title: string) => {
+    const newTask: Task = {
+      id: Math.random().toString(),
+      title,
+      isFinished: false
+    }
+    set((state: any) => [...state.tasks, newTask]);
+  }
 }));
 
 export default useTasksStore
