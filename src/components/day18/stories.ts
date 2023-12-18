e925b88c9ba96eb1e9b92d66aca131ec66f48217
Rokas <rookas.rudzenskas@gmail.com>
2023-12-18 14:47:27 +0200

    fix

---
+++ src/components/day18/stories.ts
@@ -0,0 +1,17 @@
+export const USER_STORIES = [
+  {
+    id: '1',
+    username: 'rookas',
+    stories: [],
+  },
+  {
+    id: '2',
+    username: 'toookas',
+    stories: [],
+  },
+  {
+    id: '3',
+    username: 'gooodkas',
+    stories: [],
+  }
+]
