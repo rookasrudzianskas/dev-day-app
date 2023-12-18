16df359cd667853e789c9160ce50834a8a2678b1
Rokas <rookas.rudzenskas@gmail.com>
2023-12-18 14:50:26 +0200

    fix

--- src/app/(days)/day18/stories.tsx
+++ src/app/(days)/day18/stories.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import {Stack} from "expo-router";
+import {USER_STORIES} from "@/src/components/day18/stories";
 
 const uri = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg';
 
@@ -10,7 +11,7 @@ const Stories = () => {
     <View className="">
       <Stack.Screen options={{ headerShown: false }} />
       <Image
-        source={{ uri: uri }}
+        source={{ uri: USER_STORIES[0].stories[2].uri }}
         style={styles.image}
       />
     </View>
--- src/components/day18/stories.ts
+++ src/components/day18/stories.ts
@@ -19,13 +19,13 @@ export const USER_STORIES = [
     username: 'toookas',
     stories: [
       {
-        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/4.jpeg',
       },
       {
-        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/5.jpg',
       },
       {
-        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/3.jpg',
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/6.jpg',
       }
     ],
   },
@@ -34,7 +34,7 @@ export const USER_STORIES = [
     username: 'gooodkas',
     stories: [
       {
-        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/7.jpg',
       },
       {
         uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
