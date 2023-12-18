e605dacd94994190c9d63ca592ab63999f8c798e
Rokas <rookas.rudzenskas@gmail.com>
2023-12-18 14:52:45 +0200

    fix

--- src/app/(days)/day18/stories.tsx
+++ src/app/(days)/day18/stories.tsx
@@ -1,5 +1,5 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import {Stack} from "expo-router";
 import {USER_STORIES} from "@/src/components/day18/stories";
@@ -7,11 +7,17 @@ import {USER_STORIES} from "@/src/components/day18/stories";
 const uri = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg';
 
 const Stories = () => {
+  const [userIndex, setUserIndex] = useState(0);
+  const [storyIndex, setStoryIndex] = useState(0);
+
+  const user = USER_STORIES[userIndex];
+  const story = user.stories[storyIndex];
+
   return (
     <View className="">
       <Stack.Screen options={{ headerShown: false }} />
       <Image
-        source={{ uri: USER_STORIES[0].stories[2].uri }}
+        source={{ uri: story.uri }}
         style={styles.image}
       />
     </View>
