be2e2d72b038370e9a991155ea45f8334ec05615
Rokas <rookas.rudzenskas@gmail.com>
2023-12-18 16:26:21 +0200

    fix

--- src/app/(days)/day18/stories.tsx
+++ src/app/(days)/day18/stories.tsx
@@ -2,11 +2,30 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import IGStories from "@/src/components/day18/ig-stories";
+import {Stack} from "expo-router";
 
 const Stories = () => {
+  const width = 200;
   return (
-    <View className="flex-1">
-      <IGStories />
+    <View className="flex-1 bg-black flex items-center justify-center">
+      <Stack.Screen options={{ headerShown: false }} />
+      <View style={{
+        width: width,
+        aspectRatio: 9/16,
+        backgroundColor: '#E1F3FA',
+        borderRadius: 12,
+
+        transform: [
+          {
+            perspective: 100,
+          },
+          {
+            rotateY: '45deg'
+          }
+        ]
+      }}>
+      </View>
+      {/*<IGStories />*/}
     </View>
   );
 };
