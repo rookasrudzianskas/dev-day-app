e2925fa064242f741f66295b3e5925ce69d4f66e
Rokas <rookas.rudzenskas@gmail.com>
2023-12-18 14:48:28 +0200

    fix

--- src/components/day18/stories.ts
+++ src/components/day18/stories.ts
@@ -2,16 +2,46 @@ export const USER_STORIES = [
   {
     id: '1',
     username: 'rookas',
-    stories: [],
+    stories: [
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
+      },
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
+      },
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/3.jpg',
+      }
+    ],
   },
   {
     id: '2',
     username: 'toookas',
-    stories: [],
+    stories: [
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
+      },
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
+      },
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/3.jpg',
+      }
+    ],
   },
   {
     id: '3',
     username: 'gooodkas',
-    stories: [],
+    stories: [
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
+      },
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
+      },
+      {
+        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/3.jpg',
+      }
+    ],
   }
 ]
