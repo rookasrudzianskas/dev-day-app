a2a1f8a014acd8635577e1dc9f3566cbd288ab69
Rokas <rookas.rudzenskas@gmail.com>
2023-12-20 14:55:38 +0200

    Fix

--- src/app/(days)/day20/analytics.tsx
+++ src/app/(days)/day20/analytics.tsx
@@ -1,6 +1,15 @@
 //@ts-nocheck
 import React, {useState} from 'react';
-import {Text, View, StyleSheet, TextInput, TouchableOpacity, FlatList} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
 import {Stack} from "expo-router";
 import Message from "@/src/components/day20/message";
 
@@ -39,31 +48,37 @@ const Analytics = () => {
       <Stack.Screen options={{ headerShown: false }} />
 
       <View className="h-full">
-        <View className="flex flex-1">
-          <FlatList
-            data={messages}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item, index}) => (
-              <Message message={item} />
-            )}
-          />
-        </View>
-        <View className="flex flex-row items-center w-full mb-10">
-          <TextInput
-            className="border border-neutral-300 flex-1 my-auto rounded-md p-2"
-            placeholder="Search"
-            autoCorrect={false}
-            autoCapitalize="none"
-            value={prompt}
-            onChangeText={setPrompt}
-          />
-          <TouchableOpacity
-            className="bg-primary-500 rounded-md p-2"
-            onPress={() => onSendMessage()}
-          >
-            <Text className="text-primary-500">Send</Text>
-          </TouchableOpacity>
-        </View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{flex: 1}}
+          keyboardVerticalOffset={50}
+        >
+          <View className="flex flex-1">
+            <FlatList
+              data={messages}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({item, index}) => (
+                <Message message={item} />
+              )}
+            />
+          </View>
+          <View className="flex flex-row items-center w-full mb-10">
+            <TextInput
+              className="border border-neutral-300 flex-1 my-auto rounded-md p-2"
+              placeholder="Search"
+              autoCorrect={false}
+              autoCapitalize="none"
+              value={prompt}
+              onChangeText={setPrompt}
+            />
+            <TouchableOpacity
+              className="bg-primary-500 rounded-md p-2"
+              onPress={() => onSendMessage()}
+            >
+              <Text className="text-primary-500">Send</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
