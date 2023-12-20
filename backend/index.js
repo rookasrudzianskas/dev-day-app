1f9b56218dc6f72f6081d2b0b1d7f43d809a3e33
Rokas <rookas.rudzenskas@gmail.com>
2023-12-20 15:08:00 +0200

    fix

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send("Hello from Express!")
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
--- src/app/(days)/day20/analytics.tsx
+++ src/app/(days)/day20/analytics.tsx
@@ -47,7 +47,9 @@ const Analytics = () => {
   return (
     <View className="pt-16 flex-1 mx-5">
       <Stack.Screen options={{ headerShown: false }} />
-
+      {/*<View className="bg-blue-500 px-3 rounded-full py-1">*/}
+      {/*  <Text className="text-white">Talk</Text>*/}
+      {/*</View>*/}
       <View className="h-full">
         <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : "height"}
